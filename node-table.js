1267c8a39e3e565b1c371018220d8e16e49a67b2
Danroyal001 <danroyaleffiong@gmail.com>
2020-09-29 15:19:53 +0100

    5th commit

---
+++ node-table.js
@@ -0,0 +1,22 @@
+console.table([
+    {
+        name: 'Daniel',
+        profession: 'Web Development'
+    },
+    {
+        name: 'Daniel',
+        profession: 'Web Development'
+    },
+    ['a', 'b', [
+        {
+            name: 'Daniel',
+            profession: 'Web Development'
+        },
+        {
+            name: 'Daniel',
+            profession: 'Web Development'
+        },
+        ['hi']
+    ]
+]
+])
