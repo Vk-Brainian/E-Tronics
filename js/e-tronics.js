7e41d6b18993b8d3ed55b886e99e12cf022f705f
Danroyal001 <danroyaleffiong@gmail.com>
2020-10-02 12:25:51 +0100

    9th commit

--- js/e-tronics.js
+++ js/e-tronics.js
@@ -1,22 +1,19 @@
-// $(
-//     $('form').submit((e) => {
-//         e.preventDefault();
-//         const name = $("name='name'").val();
-//         const email = $("name='email'").val();
-//         const subject = $("name='subject'").val();
-//         const number = $("name='number'").val();
-//         const message = $("name='message'").val();
+// window.submit = (e) => {
+//     e.preventDefault()
+//     const name = $('name=name').val();
+//     const email = $('name=email').val();
+//     const subject = $('name=subject').val();
+//     const number = $('name=number').val();
+//     const message = $('name=message').val();
+//     window.open().location = `mailto:e-tronics@gmail.com/?from=${email}&subject=${subject}&message=${name} \n ${email} \n ${number} \n ${message}`;
+// }
 
-//         window.location.href = `mailto:e-tronics@gmail.com/?from=${email}&subject=${subject}&message=${name} \n ${email} \n ${number} \n ${message}`;
-//     })
-// );
-
-window.submit = (e) => {
+document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
-    const name = $('name=name').val();
-    const email = $('name=email').val();
-    const subject = $('name=subject').val();
-    const number = $('name=number').val();
-    const message = $('name=message').val();
-    window.location.href = `mailto:e-tronics@gmail.com/?from=${email}&subject=${subject}&message=${name} \n ${email} \n ${number} \n ${message}`;
-}
+    const name = document.querySelector("[name=name]").value;
+    const email = document.querySelector("[name=email]").value;
+    const subject = document.querySelector("[name=subject]").value;
+    const number = document.querySelector("[name=number]").value;
+    const message = document.querySelector("[name=message]").value;
+    window.open().location = `mailto:e-tronics@gmail.com/?from=${email}&subject=${subject}&message=${name} \n ${email} \n ${number} \n ${message}`;
+});
