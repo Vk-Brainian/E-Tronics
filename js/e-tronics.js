a475e90148420143c279f487132b637d0361c22d
Danroyal001 <danroyaleffiong@gmail.com>
2020-10-02 12:08:58 +0100

    8th commit

--- js/e-tronics.js
+++ js/e-tronics.js
@@ -1,5 +1,22 @@
-$(
-    $('form').submit((e) => {
-        e.preventDefault();
-    })
-);
+// $(
+//     $('form').submit((e) => {
+//         e.preventDefault();
+//         const name = $("name='name'").val();
+//         const email = $("name='email'").val();
+//         const subject = $("name='subject'").val();
+//         const number = $("name='number'").val();
+//         const message = $("name='message'").val();
+
+//         window.location.href = `mailto:e-tronics@gmail.com/?from=${email}&subject=${subject}&message=${name} \n ${email} \n ${number} \n ${message}`;
+//     })
+// );
+
+window.submit = (e) => {
+    e.preventDefault();
+    const name = $('name=name').val();
+    const email = $('name=email').val();
+    const subject = $('name=subject').val();
+    const number = $('name=number').val();
+    const message = $('name=message').val();
+    window.location.href = `mailto:e-tronics@gmail.com/?from=${email}&subject=${subject}&message=${name} \n ${email} \n ${number} \n ${message}`;
+}
